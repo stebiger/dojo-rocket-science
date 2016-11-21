f217ebddc8e012f741ed7f1bf37e5073c9e901c2
Stefan Billeb <stefan.billeb@opitz-consulting.com>
2016-11-21 22:13:34 +0100

    Added solution for three iterations inspired bei Björn Bruhns

--- src/main/java/Launchpad.java
+++ src/main/java/Launchpad.java
@@ -1,9 +1,39 @@
+import de.oc.hg.dojo.rocketscience.rocket.Rocket;
 
 public class Launchpad {
 
 	public static void main(String[] args) {
-		// TODO Add your way of launching the rocket
-		System.err.println("No rocket found...");
+		iteration1();
+//		iteration2();
+//		iteration3();
+	}
+	
+	private static void iteration1() {
+		String rocket = new Rocket().createRockets("Hello");
+		System.out.print(rocket);
+	}
+	
+	private static void iteration2() {
+		String rockets = new Rocket().createRockets("Das Pferd frisst keinen Gurkensalat");
+		System.out.println(rockets);
+	}
+	
+	private static void iteration3() {
+		String rocket = new Rocket().createRockets("Hello");
+		System.out.print(rocket);
+		for(int i=0; i < 100000; i++) {
+			if(i%2 == 0) {
+				System.out.print("\r VvV");
+			} else {
+				System.out.print("\r vVv");
+			}
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}		
 	}
 
 }
